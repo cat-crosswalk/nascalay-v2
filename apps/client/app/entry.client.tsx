3fa3fb94c1702359bdc7317a98c228b88c39184b
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2024-05-14 01:55:05 +0900

    👕 biome かける

--- apps/client/app/entry.client.tsx
+++ apps/client/app/entry.client.tsx
@@ -1,15 +1,15 @@
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
+import { StrictMode, startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
 
 startTransition(() => {
-  const appElement = document.querySelector("#app");
-  if (appElement) {
-    hydrateRoot(
-      appElement,
-      <StrictMode>
-        <RemixBrowser />
-      </StrictMode>
-    );
-  }
+	const appElement = document.querySelector("#app");
+	if (appElement) {
+		hydrateRoot(
+			appElement,
+			<StrictMode>
+				<RemixBrowser />
+			</StrictMode>,
+		);
+	}
 });
