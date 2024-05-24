83dbb7675dc50e1e09971acaff11e888f566eb92
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2024-05-24 14:42:25 +0900

    ♻️ theme を index.tsx から re-export するように

--- apps/client/app/routes/tokens.tsx
+++ apps/client/app/routes/tokens.tsx
@@ -1,6 +1,4 @@
-import card from "@/theme/card";
-import colors from "@/theme/colors";
-import typography from "@/theme/typography";
+import { card, colors, typography } from "@/theme";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import type React from "react";
